c7f4f7db2579f001b1861d36a450e4e214db8672
Paranat Klinhom <paranat@mercular.com>
2022-07-31 08:02:57 +0700

    feat: add change project name step

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Listr from 'listr'
 import shell from 'shelljs'
 import path from 'path'
+import os from 'os'
 
 export async function createProject(options) {
   options = {
@@ -8,30 +9,33 @@ export async function createProject(options) {
     targetDirectory: options.targetDirectory || process.cwd()
   }
 
-  const currentFileUrl = import.meta.url;
-  const templateDirectory = path.resolve(
-    new URL(currentFileUrl).pathname,
-    '../../../'
-  );
+  const homeDirectory = path.join(os.homedir(), "Desktop")
 
 
   const tasks = new Listr([
     {
       title: 'Clone Repository',
       task: () => {
-        console.log(`Path: ${templateDirectory}`)
-        shell.cd(templateDirectory)
+        console.log(`🏡 Home Directory Path 🏡 | ${homeDirectory}`)
+        shell.cd(homeDirectory)
         shell.exec('git clone https://github.com/topspinppy/express-decorator-api-boilerplate')
       }
     },
     {
       title: 'Initialized Project',
       task: () => {
+        shell.cd(homeDirectory)
         shell.exec('rm -rf .git')
-        const json = require(`${templateDirectory}/express-decorator-api-boilerplate/package.json`)
+        const json = require(`${homeDirectory}/express-decorator-api-boilerplate/package.json`)
         json.name = options.projectName
         json.author = options.author
-        require('fs').writeFileSync(`${templateDirectory}/express-decorator-api-boilerplate/package.json`, JSON.stringify(json, null, 2))
+        require('fs').writeFileSync(`${homeDirectory}/express-decorator-api-boilerplate/package.json`, JSON.stringify(json, null, 2))
+      }
+    },
+    {
+      title: 'Change Project Name',
+      task: () => {
+        require('fs').renameSync(`${homeDirectory}/express-decorator-api-boilerplate`,`${homeDirectory}/${options.projectName}`)
       }
     }
   ])
